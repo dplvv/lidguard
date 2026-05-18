e29af1c4efa8a71c29a0c698a64fc72e8ea9427e
dplvv <polivaev04@gmail.com>
2026-05-18 18:55:09 +0300

    build(spm): добавлен манифест Swift Package

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "lidguard",
+    platforms: [
+        .macOS(.v13),
+    ],
+    products: [
+        .executable(name: "LidGuardGUI", targets: ["LidGuardGUI"]),
+    ],
+    targets: [
+        .executableTarget(
+            name: "LidGuardGUI",
+            path: "AppSource",
+            linkerSettings: [
+                .linkedFramework("AppKit"),
+            ]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
